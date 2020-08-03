44b73c8802f24bb54a460b4957021c52da9bbacd
ClaudetteB24 <claudettebradley24@gmail.com>
2020-08-04 11:36:34 +1200

    merged new branch

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -1,7 +1,7 @@
 public class HelloWorldExample {
 	
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Output Hello World to the user and merged branch
 		System.out.println("Hello World");
 	}
 	
